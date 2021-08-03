087f0771af6737bd5c7e8942e4fb6f0920d0241c
Kyhaiu <marcosmuce@gmail.com>
2021-08-02 23:02:44 -0300

    Created a snake game

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
 import neural_network as nn
 from sklearn.neural_network import MLPClassifier
+import snake
 
 """
     Explicar o projeto de como treinar o PONG e ver se ta dentro das especificações
 
     Para ajustar o bias estou utilizado o erro médio, já que só temos 1 output. Isso pode?
     ou tenho que treinar elemento a elemento e ir ajustando o bias?
+
+    como calcular o erro de cada camdas que não sejam input ou output
 """
 
 
@@ -16,7 +19,9 @@ class Main():
         pass
 
     def test(self):
-        rna = nn.neural_network(2, (2, 4), 1)
+
+        snake.init()
+        """rna = nn.neural_network(2, (2, 4), 1)
         x_train, y_train = [], []
         x_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
         y_train = [0, 1, 1, 0]
@@ -26,7 +31,7 @@ class Main():
         print("my rna guess for [0,0]:", nn.to_array(rna.predict([0,0])))
         print("my rna guess for [0,1]:", nn.to_array(rna.predict([0,1])))
         print("my rna guess for [1,0]:", nn.to_array(rna.predict([1,0])))
-        print("my rna guess for [1,1]:", nn.to_array(rna.predict([1,1])))
+        print("my rna guess for [1,1]:", nn.to_array(rna.predict([1,1])))"""
 
         #mlp = MLPClassifier(hidden_layer_sizes=(2, 2), activation='logistic', learning_rate='constant', max_iter=50000)
         #mlp.fit(x_train, y_train)
